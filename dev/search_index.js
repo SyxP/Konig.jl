2af1497101cd6dc44f7a0128ee0b621ddc800f5d
autodocs <autodocs>
2018-12-07 12:41:54 +0000

    build based on 0a7c658

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Konig.jl-1",
+    "page": "Home",
+    "title": "Konig.jl",
+    "category": "section",
+    "text": "Modules = [Konig]"
+},
+
+]}
