07c611d9eb79d214aa65ff972b6783455d6d406a
autodocs <autodocs>
2018-12-09 17:41:27 +0000

    build based on 112c582

--- dev/search_index.js
+++ dev/search_index.js
@@ -2,18 +2,18 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#",
-    "page": "Home",
-    "title": "Home",
+    "page": "Introduction",
+    "title": "Introduction",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "#Konig.jl-1",
-    "page": "Home",
+    "page": "Introduction",
     "title": "Konig.jl",
     "category": "section",
-    "text": "Modules = [Konig]"
+    "text": "The package Konig.jl is a library of efficient algorithms on graphs. Named after Dénes Kőnig, it uses graph-theoretic knowledge to allow algorithms to specialise on the special properties of graphs (i.e. Bipartite Graphs, Trees).The few key differences between this package and other graph algorithm packages is:The usage of (linear) algebraic techniques to speed up algorithms. In this view, adjacency matrices are simply matrices and adjacency lists can be viewed as a special encoding of a (possibly) sparse matrices.\nUsing multiple dispatch to allow algorithms to specialise on different types of graphs.\nAn experimental testbed of algorithms from theoretical computer science. This would mean that the algorithms here could be highly volatile, as we attempt to iterate and improve."
 },
 
 ]}
